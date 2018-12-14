211751f2d27e666e9903af81c39da7747a423cf8
Rafael de Oliveira <rafaextradev@gmail.com>
2018-12-14 16:49:01 -0200

    Corrigido situação dos Models.
    O caminho das pastas dentro de Media, eram o problema.

--- realtors/models.py
+++ realtors/models.py
@@ -6,10 +6,10 @@ from datetime import datetime
 
 class Realtor(models.Model):
     name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='photos/%Y/%/m/%d/', blank=True)
+    photo = models.ImageField(upload_to='photos/', blank=True)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
-    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
